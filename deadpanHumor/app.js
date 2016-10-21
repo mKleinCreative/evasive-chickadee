c9798d771c1686c64315ead774a28071814cf9cb
sdweber422 <sdweber422@gmail.com>
2016-10-21 13:27:57 -0700

    Added more quotes and fixed UI

--- deadpanHumor/app.js
+++ deadpanHumor/app.js
@@ -13,7 +13,59 @@ var quotes = [
 "I play the guitar, I taught myself how to play the guitar, which was a bad decision... because I didn't know how to play it, so I was a shitty teacher. I would never have went to me.",
 "I hate turkeys. If you go to the grocery store and you stand in front of the lunchmeat section for too long, you start to get pissed off at turkeys. You see, like, turkey ham, turkey pastrami, turkey bologna... somebody needs to tell the turkeys, \"man, just be yourself!\"",
 "I order the club sandwich all the time, but I'm not even a member, man! I don't know how I get away with it.",
-"I'm sick of your bullshit, Bootstrap!"
+"I'm sick of your bullshit, Bootstrap!",
+"I went to a restaurant that serves \"breakfast at any time\". So I ordered French Toast during the Renaissance.",
+"What's another word for Thesaurus?",
+"Everywhere is walking distance if you have the time.",
+"You can't have everything. Where would you put it?",
+"For my birthday I got a humidifier and a de-humidifier... I put them in the same room and let them fight it out.",
+"Last night I stayed up late playing poker with Tarot cards. I got a full house and four people died.",
+"I poured spot remover on my dog. Now he's gone.",
+"I intend to live forever. So far, so good.",
+"Right now I’m having amnesia and déjà vu at the same time. I think I’ve forgotten this before.",
+"I used to work in a fire hydrant factory. You couldn't park anywhere near the place.",
+"It's a small world, but I wouldn't want to paint it.",
+"I almost broke both my arms trying to hold open a revolving door for a woman.",
+"I got a new dog. He’s a paranoid retriever. He brings back everything because he’s not sure what I threw him.",
+"Every morning I get up and make instant coffee and I drink it so I have the energy to make real coffee.",
+"Woke up this morning and folded my bed back into a couch. Almost broke both my arms cause it’s not that kind of bed.",
+"I’m going to get a tattoo over my whole body of me but taller.",
+"I went to a tourist information booth and said \"tell me about some people who were here last year.\"",
+"I’ve been getting into astronomy so I installed a skylight. The people who live above me are furious.",
+"Why is it a penny for your thoughts but you have to put your two cents in? Somebody’s making a penny.",
+"I broke a mirror in my house and I’m supposed to get seven years bad luck, but my lawyer thinks he can get me five.",
+"When I get real real bored I like to drive downtown and get a great parking spot then sit in my car and count how many people ask me if I’m leaving.",
+"I spilled spot remover on my dog and now he’s gone.",
+"I’m writing a book. I have the page numbers done; now I just have to fill in the rest.",
+"When we were driving over the border back into the United States, they asked me if I had any firearms. I said what do you need?",
+"I've written several children's books ... Not on purpose.",
+"I called the wrong number today. I said \“Hello, is Joey there?\” A woman answered and she said “yes he is.” And I said ‘can I speak to him please?’ She said ‘no, he can’t talk right now, he’s only two months old.' I said \“alright, I’ll wait.”",
+"I went to a place to eat. It said \"breakfast at any time.\" So I ordered French Toast during the Renaissance.",
+"We lived in a house that ran on static electricity. If we wanted to cook something, we had to take a sweater off real quick. If we wanted to run a blender we had to rub balloons on our heads.",
+"I stayed up one night playing poker with Tarot cards. I got a full house and four people died.",
+"I was Caesarean born. Can’t really tell, although whenever I leave a house I go through the window.",
+"I bought a doughnut and they gave me a receipt for the doughnut; I don't need a receipt for the doughnut. I'll just give you the money, and you give me the doughnut, end of transaction. We don't need to bring ink and paper into this. I just can't imagine a scenario where I would have to prove that I bought a doughnut. Some skeptical friend: \"Don't even act like I didn't get that doughnut! I got the documentation right here...oh, wait it's at home...in the file...under 'D'\"",
+"A severed foot is the ultimate stocking stuffer.",
+"I am not addicted to gambling although I am addicted to sitting in a semi-circle.",
+"You know they call corn-on-the-cob \"corn-on-the-cob,\" right? But that's how it comes out of the ground, man. They should call that \"corn\", and they should call every other version \"corn-off-the-cob.\" It's not like if you cut off my arm you would call my arm \"Mitch\", but then reattach it and call it \"Mitch-all-together.\"",
+"I bought a $7 pen because I always lose pens and I got sick of not caring.",
+"I used to do drugs. I still do, but I used to, too.",
+"I hope the next time I move I get a real easy phone number, something that's real easy to remember. Something like two two two two two two two. I would say \"Sweet.\" And then people would say \"Mitch, how do I get a hold of you?\" I'd say \"Just press two for a while and when I answer, you will know you have pressed two enough.\"",
+"My roommate said to me, 'I'm gonna go shave and use the shower; does anyone need to use the bathroom?' It's like some weird ass quiz where he reveals the answer first.",
+"This is what my friend said to me; he said \"You know what I like? Mashed potatoes.\" It's like, \"Dude. you gotta give me time to guess. If you're gonna quiz me, you must insert a pause in there.\"",
+"A guy told me he liked cherries... But... I waited to see if he was gonna say tomato... Before I realized he likes cherries just... All right, that joke is ridiculous. That's like a carbon copy of the previous joke but with different ingredients. I don't know what I was trying to pull off there.",
+"Last week I helped my friend stay put. It's a lot easier'n helpin' 'em move. I just went over to his house and made sure that he did not start to load shit into a truck.",
+"Whenever I go to shave, I assume there's someone else on the planet shaving, so I say \"I'm gonna go shave, too.\"",
+"On a traffic light green means 'go' and yellow means 'yield', but on a banana it's just the opposite. Green means 'hold on,' yellow means 'go ahead,' and red means, 'where the hell did you get that banana at?'",
+"I never joined the army because \"at ease\" never seemed that easy to me. It seemed rather uptight, still. I do not relax by putting my arms behind my back and parting my legs slightly, that does not equal ease to me. At ease is not being in the military. I'm eased bro, cause I'm not in the military.",
+"My friend said to me \"I think the weather's trippy.\" I said \"No, man, it's not the weather that's trippy, perhaps it is the way that we perceive it that is indeed trippy.\" Then I thought \"Man, I should have just said 'Yeah.'\"",
+"I dressed up for the CD.",
+"I think Pringles' original intention was to make tennis balls... But on the day the rubber was supposed to show up, a truckload of potatoes came. Pringles is a laid-back company, so they just said \"Fuck it, cut em up!\"",
+"When you buy a box of Ritz crackers, on the back of the box, they have all these suggestions as to what to put on top of the Ritz. \"Try it with turkey and cheese. Try it with peanut butter.\" But I like crackers man, that's why I bought it, 'cause I like crackers! I don't see a suggestion to put a Ritz on top of a Ritz. I didn't buy them because they're little edible plates! You've got no faith in the product itself.",
+"I'm against picketing, but I don't know how to show it.",
+"I was walking down the street with my friend and he said \"I hear music\", as if there is any other way you can take it in. You're not special, that's how I receive it too. I tried to taste it but it did not work.",
+"I haven't slept for ten days, because that would be too long."
+
 ];
 
 var background = [
@@ -60,11 +112,7 @@ var lastPos = 0;
 var timer = 0;
 var backColor = 'rgba(0,0,0,.3)';
 var val = 'rgba(0,0,0,.3)';
-var timer1;
-var timer2;
-var timer3;
-var timer4;
-var timer5;
+
 
 function quote(pos) {
   return quotes[pos];
@@ -113,11 +161,11 @@ $(document).ready(function () {
     } while (val === backColor);
     $('#main-content').css('background-color', val);
     $('#main-content').css('color', backArr[1]);
-    $(".one").delay(1500).queue(function(next) {
+    $(".one").delay(1200).queue(function(next) {
       $(".one").css("background-color", val);
       next();
     });
-    $(".two").delay(1200).queue(function(next) {
+    $(".two").delay(1050).queue(function(next) {
       $(".two").css("background-color", val);
       next();
     });
@@ -125,12 +173,24 @@ $(document).ready(function () {
       $(".three").css("background-color", val);
       next();
     });
-    $(".four").delay(600).queue(function(next) {
+    $(".four").delay(750).queue(function(next) {
       $(".four").css("background-color", val);
       next();
     });
-    $(".five").delay(300).queue(function(next) {
+    $(".five").delay(600).queue(function(next) {
       $(".five").css("background-color", val);
+      next();
+    });
+    $(".six").delay(450).queue(function(next) {
+      $(".six").css("background-color", val);
+      next();
+    });
+    $(".seven").delay(300).queue(function(next) {
+      $(".seven").css("background-color", val);
+      next();
+    });
+    $(".eight").delay(150).queue(function(next) {
+      $(".eight").css("background-color", val);
       next();
     });
     backColor = val;
